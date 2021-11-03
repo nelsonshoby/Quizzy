7633a5afdbfaffc83c79e40f2f2f133a60cf6125
nelson shoby <nelsonshoby009@gmail.com>
2021-11-03 15:45:57 +0530

    Added rake task setup

--- app/models/User.rb
+++ app/models/User.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   enum role: { standard: 0, administrator: 1 }
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :first_name, :last_name, presence: true, length: { maximum: Constants::MAX_USERNAME_LENGTH }
-  validates :password, length: { minimum: 6 }, if: -> { password.present? && role == "standard" }
+  validates :password, length: { minimum: 6 }, if: -> { password.present? }
   validates :password_confirmation, presence: true, on: :create
 
   before_save :to_lower_case
