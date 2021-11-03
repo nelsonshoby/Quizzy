58bbe494f34b7a0e5c2028e155a1d6d663ad0da6
nelson shoby <nelsonshoby009@gmail.com>
2021-11-03 13:51:19 +0530

    Added secure password to user and did unit testing for the same

--- app/models/User.rb
+++ app/models/User.rb
@@ -2,10 +2,12 @@
 
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
-
+  has_secure_password
   enum role: { standard: 0, administrator: 1 }
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :first_name, :last_name, presence: true, length: { maximum: Constants::MAX_USERNAME_LENGTH }
+  validates :password, length: { minimum: 6 }, if: -> { password.present? && role == "standard" }
+  validates :password_confirmation, presence: true, on: :create
 
   before_save :to_lower_case
 
