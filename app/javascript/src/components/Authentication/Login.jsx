532487e464d9f417e152dd1c4deecd1b3e532360
nelson shoby <nelsonshoby009@gmail.com>
2021-11-19 19:15:24 +0530

    Created folder authetication

---
+++ app/javascript/src/components/Authentication/Login.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+
+import { Input } from "@bigbinary/neetoui/v2";
+import { Button } from "@bigbinary/neetoui/v2";
+import { Header } from "@bigbinary/neetoui/v2/layouts";
+
+import authApi from "apis/auth";
+
+import { setAuthHeaders } from "../../apis/axios";
+import { setToLocalStorage } from "../../helpers/storage";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  loading;
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      const response = await authApi.login({ login: { email, password } });
+      setToLocalStorage({
+        authToken: response.data.authentication_token,
+        authEmail: response.data.email,
+        userId: response.data.id,
+        userName: response.data.first_name + " " + response.data.last_name,
+      });
+      setAuthHeaders();
+      setLoading(false);
+      window.location.href = "/";
+    } catch (error) {
+      logger.error(error);
+      setLoading(false);
+    }
+  };
+  return (
+    <div>
+      <div className="border-b-2">
+        <Header title="Quizzy" className="ml-10" />
+      </div>
+      <div className="mx-64">
+        <div className="flex items-center justify-center mt-20 border-gray-100 border-8 mx-64 shadow-xl">
+          <div className="w-full max-w-md m-8">
+            <h2
+              className="mt-6 text-3xl font-extrabold leading-9
+            text-center text-bb-gray-700"
+            >
+              Log In
+            </h2>
+            <div className=" text-center">
+              <form onSubmit={handleSubmit}>
+                <Input
+                  value={email}
+                  label="Email"
+                  placeholder="Enter Email"
+                  onChange={e => setEmail(e.target.value)}
+                />
+                <Input
+                  value={password}
+                  type="password"
+                  label="Password"
+                  placeholder="Enter Password"
+                  className="py-4"
+                  onChange={e => setPassword(e.target.value)}
+                />
+                <Button
+                  type="submit"
+                  className="mt-4"
+                  label="Submit"
+                  style="primary"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
